e7032a4863275085094c194268285c8b553bdd22
lhakiim <luqmanulhakim2021@gmail.com>
2024-09-11 16:42:21 +0700

    Tutorial 2 Selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://luqmanul-hakim31-mentalhealthtracker.pbp.cs.ui.ac.id/", "http://luqmanul-hakim31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://luqmanul-hakim31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://luqmanul-hakim31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
